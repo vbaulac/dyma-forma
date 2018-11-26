f9290d1cbb30ae3877626155a81205309c9ecdc4
Victor Baulac <victor.baulac@gmail.com>
2018-11-26 10:18:35 +0100

    togglebar (fail)

--- animations/src/app/toggle/toggle.component.ts
+++ animations/src/app/toggle/toggle.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.component.html',
-  styleUrls: ['./toggle.component.css']
+  styleUrls: ['./toggle.component.css'],
+  animations: [
+    trigger('nav', [
+      state('show', style({
+        width:'200px',
+        background: 'red'       
+      })),
+      state('hide', style({
+        width:'0px',
+        background: 'blue'
+      })),
+      transition('show <=> hide', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class ToggleComponent implements OnInit {
+  public state: string = 'show';
+
+  public changeState() {
+    console.log(this.state);
+    this.state = (this.state === 'show') ? 'hide' : 'show';
+    console.log(this.state);
+  }
 
   constructor() { }
 
